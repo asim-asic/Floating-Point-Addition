cc9fcad3e644b1135e8f31867eecb9a1830c418b
Asim <261854487+asim-asic@users.noreply.github.com>
2026-04-08 00:52:03 +0530

    Update design file

--- fp.v
+++ fp.v
@@ -3,14 +3,22 @@ module fp_addition(
 	input start,	// start signal to begin operation
 	input [31:0] fp_input, // 32-bit floating point input 
 	
-	output reg [31:0], fp_sum,	// final floating point result
+	output [31:0] fp_sum,	// final floating point result
 	output reg done,	// operation complete flag
 	output reg overflow,	// exponent overflow flag
 	output reg underflow	// exponent underflow flag
 );
 
+	//-------------------------- FSM States ------------------------
+	parameter IDLE = 3'd0,
+		  LOAD_B = 3'd1,
+		  ALIGN = 3'd2,
+		  ADD = 3'd3,
+		  NORMALIZE = 3'd4,
+		  CHECK = 3'd5,
+		  DONE = 3'd6;
 
-	//------------- Internal signals --------------------
+	//--------------------------- REGISTER -------------------------
 	reg [25:0] frac_a;	// fraction of component A
 				/* 23 bit fraction + 1 hidden bit + extra bits for alignment*/ 
 	reg [25:0] frac_b;	// fraction of component B
@@ -22,7 +30,7 @@ module fp_addition(
 	reg sign_b;		// sign of component B
 	
 
-	// ------------------ WIRES ----------------------
+	// -------------------------- WIRES ---------------------------
 	wire frac_overflow;	// fraction overflow (after addition)
 	wire frac_underflow;	// fraction underflow or normalization required
 
@@ -33,7 +41,7 @@ module fp_addition(
 
 	wire [27:0] frac_sum;	// final fraction after normalization
 	
-	// -------------------- Control ----------------------------
+	// ------------------------ Control ----------------------------
 	reg [2:0] state;	// FSM state register
 	
 
@@ -42,7 +50,7 @@ module fp_addition(
 	// Initial block
 	initial
 	begin
-		state = 0;
+		state = IDLE;
 		done = 0;
 		overflow = 0;
 		underflow = 0;
@@ -50,25 +58,23 @@ module fp_addition(
 		frac_b = 0;
 		exp_a = 0;
 		exp_b = 0;
-		sign = 0;
-		sign = 0;
+		sign_a = 0;
+		sign_b = 0;
 	end
 	
 
-	// 2's complement
+	// 2's complement (if negative)
 	assign frac_a_comp = (sign_a == 1'b1) ? ~({2'b00, frac_a}) + 1 : {2'b00, frac_a}; 
 	assign frac_b_comp = (sign_b == 1'b1) ? ~({2'b00, frac_b}) + 1 : {2'b00, frac_b};
 	
-	// Add fraction
+	// Add
 	assign add_out = (frac_a_comp + frac_a_comp);	
 	
-	// Convert back to magnitude if result negative
-	assign frac_sum = ((add_out[27] == 1'b0) ? add_out : ~add_out + 1;
+	// back to magnitude
+	assign frac_sum = (add_out[27] == 1'b0) ? add_out : ~add_out + 1;
 	
-	// overflow (need right shift)
+	// overflow or underflow detect
 	assign frac_overflow = frac_sum[27] ^ frac_sum[26];
-	
-	// underflow (need left shift)
 	assign frac_underflow = ~frac_a[25];
 
 	// pack final result
@@ -78,8 +84,8 @@ module fp_addition(
 	// -------------------------------------- FSM -----------------------
 	always @(posedge clk)
 	begin	
-		case(start)
-			0: begin
+		case(state)
+		    IDLE: begin
 				if(start == 1'b1)
 				begin
 					exp_a <= fp_input[30:23];
@@ -98,8 +104,12 @@ module fp_addition(
 					underflow <= 1'b0;
 					state <= 1;
 				end
+				else
+				begin
+					state <= IDLE;
+				end
 			   end	   
-			1: begin
+		   LOAD_B: begin
 				exp_b <= fp_input[30:23];
 				sign_b <= fp_input[31];
 				frac_b[24:0] <= {fp_input[22:0], 2'b00};
@@ -111,19 +121,18 @@ module fp_addition(
 				begin
 					frac_b[25] <= 1'b1;
 				end
-				state <= 2;
-				end
+				state <= ALIGN;
 			   end
-			2: begin
+		    ALIGN: begin
 				if(frac_a == 0 | frac_b == 0)
 				begin
-					state <= 3;
+					state <= ADD;
 				end
 				else
 				begin
 					if(exp_a == exp_b)
 					begin
-						state <= 3;
+						state <= ADD;
 					end
 					else if(exp_a < exp_b)
 					begin
@@ -132,12 +141,12 @@ module fp_addition(
 					end
 					else
 					begin
-						frac_b <= {1'b0, frac_2[25:1]};
+						frac_b <= {1'b0, frac_b[25:1]};
 						exp_b <= exp_b + 1;
 					end
 				end
 			   end
-		        3: begin
+		      ADD: begin
 				sign_a <= add_out[27];
 				if(frac_overflow == 1'b0)
 				begin
@@ -148,36 +157,36 @@ module fp_addition(
 					frac_a <= frac_sum[26:1];
 					exp_a <= exp_a + 1;
 				end
-				state <= 4;
+				state <= NORMALIZE;
 			   end
-			4: begin
+		NORMALIZE: begin
 			   	if(frac_a == 0)
 				begin
 					exp_a <= 8'b0000_0000;
-					state <= 6;
+					state <= DONE;
 				end
 				else
 				begin
-					state <= 5;
+					state <= CHECK;
 				end
 			   end
-			5: begin
+		    CHECK: begin
 				if(exp_a == 0)
 				begin
 					underflow <= 1'b1;
-					state <= 6;
+					state <= DONE;
 				end
 				else if (frac_overflow == 1'b0)
 				begin
-					state <= 6;
+					state <= DONE;
 				end
 				else
 				begin
-					frac_a <= {frac[24:0], 1'b0};
+					frac_a <= {frac_a[24:0], 1'b0};
 					exp_a <= exp_a - 1;
 				end
 			   end
-			6: begin
+		     DONE: begin
 			 	if(exp_a == 255)
 				begin
 					overflow <= 1'b1;
@@ -185,6 +194,9 @@ module fp_addition(
 				done <= 1'b1;
 				state <= 0;	
 			   end
+		  default: begin
+				state <= IDLE;
+		  	   end
 		endcase
 	end
 endmodule
